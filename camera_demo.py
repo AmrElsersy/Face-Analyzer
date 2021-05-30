fd582b3ba45d2237ebf71fc3cd9e72de31d4dfc4
AmrElsersy <amrelsersay@gmail.com>
2021-05-31 01:06:33 +0200

    Gaze Tracking
    Signed-off-by: AmrElsersy <amrelsersay@gmail.com>

--- camera_demo.py
+++ camera_demo.py
@@ -6,19 +6,20 @@ import torch
 from face_detector.face_detector import DnnDetector, HaarCascadeDetector
 from utils import normalization, histogram_equalization, standerlization
 from face_alignment.face_alignment import FaceAlignment
-from emotion_recognizer.emotion_recognition import recognize_face
+# from emotion_recognizer.emotion_recognition import recognize_face
+from GazeTracking.focusDetection import focusDetector
 
 sys.path.insert(1, 'face_detector')
+sys.path.insert(2, 'GazeTracking')
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_emotion(face):
-    return recognize_face(face)
+    return 'ray2'
+    # return recognize_face(face)
 
-def get_focus(face):
-    return True
-    
 def main(args):
     face_alignment = FaceAlignment()
+    focus_detector = focusDetector()
 
     # Face detection
     root = 'face_detector'
@@ -34,12 +35,12 @@ def main(args):
     isOpened = False
     if not args.image:
         if args.path:
-            video = cv2.VideoCapture(args.path) 
+            video = cv2.VideoCapture(args.path)
         else:
             video = cv2.VideoCapture(0) # 480, 640
         isOpened = video.isOpened()
         print('video.isOpened:', isOpened)
-    
+
     t1 = 0
     t2 = 0
     min_time = 1 / args.fps
@@ -50,7 +51,7 @@ def main(args):
             frame = cv2.imread(args.path)
         else:
             _, frame = video.read()
-            isOpened = video.isOpened()    
+            isOpened = video.isOpened()
         # if loaded video or image (not live camera) .. resize it (helpful in mobile camera with has different resolution)
         if args.path:
             frame = cv2.resize(frame, (640, 480))
@@ -73,8 +74,15 @@ def main(args):
 
                 # preprocessing
                 input_face = face_alignment.frontalize_face(face, frame)
-                emotion_prob, emotion_label = get_emotion(input_face)
-                focus = get_focus(input_face)
+
+                print(input_face.shape)
+                # input_face = cv2.cvtColor(input_face, cv2.COLOR_BGR2GRAY)
+
+                emotion_label = get_emotion(input_face)
+                # emotion_prob, emotion_label = get_emotion(input_face)
+                focus = ''
+                focus = focus_detector.focused(input_face)
+
                 info = {
                     'emotion': emotion_label,
                     'focus': focus
@@ -83,25 +91,34 @@ def main(args):
                 faces_info.append(info)
 
                 cv2.imshow('input face', cv2.resize(input_face, (120, 120)))
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 200, 200), 3)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 100, 0), 3)
                 cv2.putText(
                     frame,
-                    "{} {}".format(emotion_label, int(emotion_prob * 100)),
-                    (x+w, y + 1),
+                    focus,
+                    (x, y + 1),
                     cv2.FONT_HERSHEY_SIMPLEX,
                     0.8,
                     (0, 200, 200),
                     2,
                 )
+                # cv2.putText(
+                #     frame,
+                #     "{} {}".format(emotion_label, int(emotion_prob * 100)),
+                #     (x+w, y + 1),
+                #     cv2.FONT_HERSHEY_SIMPLEX,
+                #     0.8,
+                #     (0, 200, 200),
+                #     2,
+                # )
 
-        # draw FPS     
+        # draw FPS
         cv2.putText(frame, str(fps), (10,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0))
-        cv2.imshow("Video", frame)   
+        cv2.imshow("Video", frame)
         if cv2.waitKey(1) & 0xff == 27:
             if not args.image:
                 video.release()
             break
-    
+
     return faces_info
 
 if __name__ == '__main__':
@@ -109,7 +126,7 @@ if __name__ == '__main__':
     parser.add_argument('--haar', action='store_true', help='run the haar cascade face detector')
     parser.add_argument('--path', type=str, default='', help='path to video to test')
     parser.add_argument('--image', action='store_true', help='specify if you test image or not')
-    parser.add_argument('--fps', type=int, default=30, help='num of frames per second to capture info')    
+    parser.add_argument('--fps', type=int, default=30, help='num of frames per second to capture info')
     args = parser.parse_args()
 
     faces_info = main(args)
